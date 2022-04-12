e4cf987fefa08b6217b19025213fdb64c6a90a95
jodu07 <jhonnat.dussan@gmail.com>
2022-04-12 01:16:49 -0500

    clientService

--- src/app/client/services/client.service.ts
+++ src/app/client/services/client.service.ts
@@ -1,9 +1,72 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from '@angular/fire/compat/firestore';
+import { Observable } from 'rxjs';
+import { Client } from '../interface/client.interface';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClientService {
 
-  constructor() { }
+    clientsColeccion!: AngularFirestoreCollection<Client>;
+    clientDoc!: AngularFirestoreDocument<Client>;
+    clients!: Observable<Client[]>;
+    client!: Observable<Client>;
+
+  constructor( private db: AngularFirestore) {
+    this.clientsColeccion = db.collection('clientes', ref => ref.orderBy('nombre', 'asc'));
+   }
+
+
+   getClients(): Observable<Client[]>{
+    // obtener clientes
+    
+    this.clients = this.clientsColeccion.snapshotChanges().pipe(
+        map( cambios => {
+            return cambios.map( accion => {
+                const DATOS = accion.payload.doc.data() as Client;
+                DATOS.id = accion.payload.doc.id;
+                return DATOS;
+                
+            })
+        })
+    );
+    return this.clients; 
+  }  
+  
+  addClient(cliente: Client){
+    this.clientsColeccion.add(cliente);
+}
+
+//metodo para llamar cliente desde la bd y editarlo..... recuperar el documento con el id
+
+ getClient(id: string): Observable<Client>{
+    this.clientDoc = this.db.doc<Client>(`clientes/${id}`);
+    return this.client = this.clientDoc.snapshotChanges().pipe(
+        map( accion => {
+            if(accion.payload.exists === false){
+                return null;
+            }
+            else{
+                const DATOS = accion.payload.data() as Client;
+                DATOS.id = accion.payload.id;
+                return DATOS as any;
+            }
+        })
+    );
+    return this.client;
+    }
+
+    updateClient(cliente: Client){
+      this.clientDoc = this.db.doc(`clientes/${cliente.id}`);
+      this.clientDoc.update(cliente);
+  }
+
+  deleteClient(cliente: Client){
+      this.clientDoc = this.db.doc(`clientes/${cliente.id}`);
+      this.clientDoc.delete();
+  }
+
+
 }
