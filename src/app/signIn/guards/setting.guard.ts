08a1d8690cea911ca69e1a65f5b3b959994c82bd
jodu07 <jhonnat.dussan@gmail.com>
2022-04-15 01:43:41 -0500

    guard setting

--- src/app/signIn/guards/setting.guard.ts
+++ src/app/signIn/guards/setting.guard.ts
@@ -1,15 +1,29 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { SettingService } from '../services/setting.service';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SettingGuard implements CanActivate {
-  canActivate(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
-  }
-  
+  constructor(
+    private router: Router,
+    private _settingService: SettingService
+){}
+
+canActivate(): Observable<boolean>{
+    return this._settingService.getConfiguracion().pipe(
+        map( configuracion => {
+            if(configuracion.permitirRegistro){
+                return true;
+            }
+            else{
+                this.router.navigate(['/login']);
+                return false;
+            }
+        })
+    );
+}
 }
